8edf546f4f84617f5879a3db40a874fb27880800
Jeffrey Witt <jcwitt@JeffsComputer.local>
2017-06-19 20:23:24 -0400

    major progress with styling; landing page, and rubric description page

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'https://rubygems.org'
+ruby "2.3.1"
+
+gem 'nokogiri'
+gem 'sinatra'
+gem 'sinatra-contrib'
+gem 'httparty'
+gem 'puma'
+gem 'mongo'
+gem 'bson_ext'
+gem "digest"
+
+group :development do
+	gem 'pry'
+  #gem 'ruby-debug-ide'
+  #gem 'thin'
+end
---
+++ app.rb
@@ -0,0 +1,144 @@
+require 'sinatra'
+require 'securerandom'
+require 'mongo'
+require 'json/ext' # required for .to_json
+require "digest"
+require 'uri'
+require 'httparty'
+
+
+
+configure do
+  db = Mongo::Client.new(['127.0.0.1:27017'], :database => "test")
+  set :mongo_db, db[:test]
+  set :server, :puma
+  set :bind, "0.0.0.0"
+  set :protection, except: [:frame_options, :json_csrf]
+  set :root, File.dirname(__FILE__)
+
+  # this added in attempt to "forbidden" response when clicking on links
+  #set :protection, :except => :ip_spoofing
+  #set :protection, :except => :json
+end
+
+if settings.development?
+  require 'pry'
+end
+
+get '/' do
+  erb :index
+end
+get '/rubric/:society' do |society|
+  if society == "maa"
+    erb :maa_rubric
+  end
+end
+get '/reviews' do
+  @reviews = []
+
+  db = settings.mongo_db
+  db.find().map {|object|
+    @reviews << object
+  }
+  erb :reviews
+end
+get '/about' do
+  erb :about
+end
+# find a document by its Mongo ID
+get '/document/:id/?' do
+  content_type :json
+  document_by_id(params[:id])
+end
+
+get '/reviews/create' do
+  erb :create
+end
+
+post '/reviews/create' do
+  id = SecureRandom.uuid
+  date = Time.new
+  review_text_url = params[:review_text_url]
+  review_society = params[:review_society]
+  review_summary = params[:review_summary]
+  review_badge_number = params[:review_badge_number]
+  if review_badge_number == "1"
+    review_badge = "http://localhost:4567/maa-badge-working.svg"
+    badge_rubric = "http://localhost:4567/rubric/maa#green"
+  elsif review_badge_number == "2"
+    review_badge = "http://localhost:4567/maa-badge.svg"
+    badge_rubric = "http://localhost:4567/rubric/maa#gold"
+  end
+
+  response = HTTParty.get(review_text_url)
+  shasum = Digest::SHA2.hexdigest(response.body)
+
+
+  review_content =  {
+      "@id": "http://digitallatin.org/reviews/#{id}",
+      "review-metadata":
+      {
+          "review-society": review_society,
+          "date": date,
+          "badge-url": review_badge,
+          "badge-rubric": badge_rubric,
+          "review-report": "http://digitallatin.org/#{id}/report",
+          "review-summary": review_summary,
+          "origin-source": {
+            "github-url": review_text_url,
+            "shasum": shasum,
+          },
+          "dll-source": {
+            "git-url": review_text_url,
+            "shasum": shasum,
+          }
+      }
+  }
+  filename = "public/" + id + '.json'
+  final_content = JSON.pretty_generate(review_content)
+  db = settings.mongo_db
+  db.insert_one(review_content)
+  File.open(filename, 'w') { |file|
+    file.write(final_content)
+  }
+  @id = id
+  erb :create_completed
+
+end
+
+get '/reviews/:id.json' do |id|
+  headers( "Access-Control-Allow-Origin" => "*")
+  content_type :json
+  db = settings.mongo_db
+  document = db.find( { "@id": "http://digitallatin.org/reviews/#{id}" } ).to_a.first
+  (document || {}).to_json
+
+end
+get '/reviews/:id.html' do |id|
+  db = settings.mongo_db
+  @document = db.find( { "@id": "http://digitallatin.org/reviews/#{id}" } ).to_a.first
+  @id = @document["@id"].to_s.split("http://digitallatin.org/reviews/").last()
+  erb :show
+end
+
+helpers do
+  # a helper method to turn a string ID
+  # representation into a BSON::ObjectId
+  def object_id val
+    begin
+      BSON::ObjectId.from_string(val)
+    rescue BSON::ObjectId::Invalid
+      nil
+    end
+  end
+
+  def document_by_id id
+    id = object_id(id) if String === id
+    if id.nil?
+      {}.to_json
+    else
+      document = settings.mongo_db.find(:_id => id).to_a.first
+      (document || {}).to_json
+    end
+  end
+end

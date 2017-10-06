769f8b4ca2845fdcd2d2694b6b1bd3b36958f021
Jeffrey Witt <jcwitt@JeffsComputer.local>
2017-10-06 10:37:39 -0400

    added swagger documentation, added login button in footer, added new api route that computes file hash and then rediredcts

--- app.rb
+++ app.rb
@@ -33,6 +33,7 @@ configure do
   set :bind, "0.0.0.0"
   set :protection, except: [:frame_options, :json_csrf]
   set :root, File.dirname(__FILE__)
+  set :public_folder, 'public'
 
   # this added in attempt to "forbidden" response when clicking on links
   #set :protection, :except => :ip_spoofing
@@ -50,7 +51,7 @@ end
 
 def authenticate!
   client = Octokit::Client.new
-  scopes = ['repo', 'user']
+  scopes = ['user']
   url = client.authorize_url(CLIENT_ID, :scope => 'repo')
 
   redirect url
@@ -93,6 +94,10 @@ get '/login' do
     redirect '/'
   end
 end
+get '/logout' do
+  session.clear
+  redirect '/'
+end
 get '/return' do
   # get code return from github and get access token
   session_code = request.env['rack.request.query_hash']['code']
@@ -133,6 +138,9 @@ end
 get '/about' do
   erb :about
 end
+get '/docs' do
+  redirect 'docs/index.html'
+end
 # find a document by its Mongo ID
 get '/document/:id/?' do
   content_type :json
@@ -149,66 +157,64 @@ end
 
 post '/reviews/create' do
   if authorized?
+    if params[:review_text_url].include? "master"
+      @message = "sorry, it looks like you've used a github branch url, please is a url with the file blob hash"
+      @success = false
+      erb :create_completed
+    else
+      id = SecureRandom.uuid
+      date = Time.new
+      review_text_url = params[:review_text_url]
+      review_society = params[:review_society]
+      review_summary = params[:review_summary]
+      review_badge_number = params[:review_badge_number]
+      if review_badge_number == "1"
+        review_badge = "http://dll-review-registry.herokuapp.com/maa-badge-working.svg"
+        badge_rubric = "http://dll-review-registry.herokuapp.com/rubric/maa#green"
+      elsif review_badge_number == "2"
+        review_badge = "http://dll-review-registry.herokuapp.com/maa-badge.svg"
+        badge_rubric = "http://dll-review-registry.herokuapp.com/rubric/maa#gold"
+      end
 
-    id = SecureRandom.uuid
-    date = Time.new
-    review_text_url = params[:review_text_url]
-    review_society = params[:review_society]
-    review_summary = params[:review_summary]
-    review_badge_number = params[:review_badge_number]
-    if review_badge_number == "1"
-      review_badge = "http://dll-review-registry.herokuapp.com/maa-badge-working.svg"
-      badge_rubric = "http://dll-review-registry.herokuapp.com/rubric/maa#green"
-    elsif review_badge_number == "2"
-      review_badge = "http://dll-review-registry.herokuapp.com/maa-badge.svg"
-      badge_rubric = "http://dll-review-registry.herokuapp.com/rubric/maa#gold"
+      response = HTTParty.get(review_text_url)
+      shasum = Digest::SHA2.hexdigest(response.body)
+
+
+      filename = review_text_url.split('/').last
+      File.open("tmp/#{filename}", 'w') { |file|
+        file.write(response.body)
+      }
+      puts "IPFS test"
+      ipfs_report = `ipfs add "tmp/#{filename}"`
+      puts ipfs_report
+      ipfs_hash = ipfs_report.split(" ")[1]
+
+
+      review_content =  {
+          "id": id,
+          "review-society": review_society,
+          "date": date,
+          "badge-url": review_badge,
+          "badge-rubric": badge_rubric,
+          "review-report": nil,
+          "review-summary": review_summary,
+          "sha-256": shasum,
+          "ipfs-hash": ipfs_hash,
+          "git-blob-hash": nil,
+          "url": review_text_url
+      }
+      #filename = "public/" + id + '.json'
+      #final_content = JSON.pretty_generate(review_content)
+      db = settings.mongo_db
+      db.insert_one(review_content)
+      #File.open(filename, 'w') { |file|
+      #  file.write(final_content)
+      #}
+      @id = id
+      @success = true
+      @message = "Congratulations, Review Created"
+      erb :create_completed
     end
-
-    response = HTTParty.get(review_text_url)
-    shasum = Digest::SHA2.hexdigest(response.body)
-
-    digest = Digest::SHA256.digest(response.body)
-    multihash_binary_string = Multihashes.encode digest, 'sha2-256'
-
-
-    hexmulti = multihash_binary_string.unpack('H*').first
-    puts hexmulti
-
-
-
-
-    filename = review_text_url.split('/').last
-    File.open("tmp/#{filename}", 'w') { |file|
-      file.write(response.body)
-    }
-    puts "IPFS test"
-    ipfs_report = `ipfs add "tmp/#{filename}"`
-    puts ipfs_report
-    ipfs_hash = ipfs_report.split(" ")[1]
-
-
-    review_content =  {
-        "id": id,
-        "review-society": review_society,
-        "date": date,
-        "badge-url": review_badge,
-        "badge-rubric": badge_rubric,
-        "review-report": nil,
-        "review-summary": review_summary,
-        "sha-256": shasum,
-        "ipfs-hash": ipfs_hash,
-        "git-blob-hash": nil,
-        "url": review_text_url
-    }
-    #filename = "public/" + id + '.json'
-    #final_content = JSON.pretty_generate(review_content)
-    db = settings.mongo_db
-    db.insert_one(review_content)
-    #File.open(filename, 'w') { |file|
-    #  file.write(final_content)
-    #}
-    @id = id
-    erb :create_completed
   else
     "not authorized"
   end
@@ -251,7 +257,7 @@ get '/hash/:hash.html' do |id|
   erb :show_array
 end
 # api/v1 routes
-get '/api/v1/text/:hash' do |id|
+get '/api/v1/reviews/:hash' do |id|
   headers( "Access-Control-Allow-Origin" => "*")
   content_type :json
   db = settings.mongo_db
@@ -301,6 +307,13 @@ get '/api/v1/review/:id' do |id|
       "url": doc["url"]
   }.to_json
 end
+get '/api/v1/text' do
+  url = params[:url]
+  response = HTTParty.get(url)
+  shasum = Digest::SHA2.hexdigest(response.body)
+  redirect "/api/v1/reviews/#{shasum}"
+end
+
 helpers do
   # a helper method to turn a string ID
   # representation into a BSON::ObjectId
